a40ea4c7e2debfda04ba9eb01a4aa29ba7531ce2
Nick How <nickhow@nicks-mbp.home>
2020-07-15 14:28:30 +0100

    format recipe view

--- components/Recipe.vue
+++ components/Recipe.vue
@@ -7,36 +7,28 @@
       @dblclick="like"
     >
       <div class="columns">
-        <div class="column">
+        <div class="column has-text-centered">
           <h1>{{recipe.title}}</h1>
         </div>
       </div>
       <div class="columns">
-        <div class="column">
+        <div class="column has-text-centered">
           <i class="fas fa-users" />
           <span>serves {{recipe.servings}}</span>
         </div>
       </div>
-      <div class="columns">
-        <div class="column">
-          <div class="content">
-            <div class="heart">
-              <i
-                class="far fa-heart fa-lg"
-                :class="{'fas': this.recipe.hasBeenLiked}"
-                @click="like"
-              ></i>
-            </div>
-            <p class="likes">{{recipe.aggregateLikes}} likes</p>
+      <div class="columns is-mobile">
+        <div class="column has-text-centered">
+          <div class="heart">
+            <i class="far fa-heart fa-lg" :class="{'fas': this.recipe.hasBeenLiked}" @click="like"></i>
           </div>
+          <p class="likes">{{recipe.aggregateLikes}} likes</p>
         </div>
-        <div class="column">
-          <div class="content">
-            <div class="heart center">
-              <i class="far fa-clock fa-lg"></i>
-            </div>
-            <p class="likes">{{recipe.readyInMinutes}} likes</p>
+        <div class="column has-text-centered">
+          <div class="heart center">
+            <i class="far fa-clock fa-lg"></i>
           </div>
+          <p class="likes">{{recipe.readyInMinutes}} likes</p>
         </div>
       </div>
     </div>
